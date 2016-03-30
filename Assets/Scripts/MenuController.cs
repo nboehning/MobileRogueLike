5d8e3ee6cbf178174bbd2096d9bbcb8427589324
svc-student <svc-student@student.directory.jccc>
2016-03-29 19:16:24 -0500

    Settings completed
    Menu swiping and setting of settings is complete

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    // Variables for scene management
+    public GameObject[] sceneOrganizers;
+    int curMenu = 0;
+
+    // Variables for swiping
+    Vector2 prevPosition;
+    Vector2 curPosition;
+    float touchDelta;
+    int iComfort = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.touchCount == 1)
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                prevPosition = Input.GetTouch(0).position;
+            }
+            if(Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                curPosition = Input.GetTouch(0).position;
+
+                touchDelta = curPosition.magnitude - prevPosition.magnitude;
+
+                if(Mathf.Abs(touchDelta) > iComfort)
+                {
+                    if(touchDelta > 0)
+                    {
+                        if(Mathf.Abs(curPosition.x - prevPosition.x) > Mathf.Abs(curPosition.y - prevPosition.y))
+                        {
+                            // Right
+                            // Set current menu to false
+                            sceneOrganizers[curMenu].SetActive(false);
+
+                            // Move to menu to the right of current menu
+                            if (curMenu == sceneOrganizers.Length - 1)
+                            {
+                                curMenu = 0;
+                            }
+                            else
+                            {
+                                if (curMenu == 2)
+                                {
+                                    GameObject.Find("GameValueSingleton").GetComponent<SettingsSingleton>().SetSettings();
+                                }
+                                curMenu++;
+                            }
+                            // Set new menu to true
+                            sceneOrganizers[curMenu].SetActive(true);
+                        }
+                        else
+                        {
+                            // Top
+                        }
+                    }
+                    else
+                    {
+                        if(Mathf.Abs(curPosition.x - prevPosition.x) > Mathf.Abs(curPosition.y - prevPosition.y))
+                        {
+                            // Left
+                            // Set current menu to false
+                            sceneOrganizers[curMenu].SetActive(false);
+
+                            // Move to menu to the left of current menu
+                            if (curMenu == 0)
+                            {
+                                curMenu = sceneOrganizers.Length - 1;
+                            }
+                            else
+                            {
+                                if(curMenu == 2)
+                                {
+                                    GameObject.Find("GameValueSingleton").GetComponent<SettingsSingleton>().SetSettings();
+                                }
+                                curMenu--;
+                            }
+                            // Set new menu to true
+                            sceneOrganizers[curMenu].SetActive(true);
+                        }
+                        else
+                        {
+                            // Down
+                        }
+                    }
+                }
+            }
+
+        }
+	}
+
+    public void BtnPlayEndless()
+    {
+        //SceneManager.LoadScene();
+    }
+
+    public void BtnPlayProgression()
+    {
+        //SceneManager.LoadScene();
+    }
+
+    public void BtnExitGame()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SettingsSingleton.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum gameDifficulty
+{
+    EASY,
+    MEDIUM,
+    HARD
+};
+
+public class SettingsSingleton : MonoBehaviour {
+    // Variables to carry over
+    int volume = 50;
+    bool isSwipeShoot = true;
+    bool isSwipeMove = true;
+    gameDifficulty curDifficulty;
+
+    // Gameobjects that set settings
+    GameObject volumeSlider;
+    GameObject shootTapToggle;
+    GameObject shootSwipeToggle;
+    GameObject moveSwipeToggle;
+    GameObject moveTiltToggle;
+    GameObject easyToggle;
+    GameObject medToggle;
+    GameObject hardToggle;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            // Main menu scene is loaded
+            // Find all the settings setters
+            volumeSlider = GameObject.Find("VolumeSlider");
+            shootSwipeToggle = GameObject.Find("ShootSwipeToggle");
+            moveSwipeToggle = GameObject.Find("MoveSwipeToggle");
+            easyToggle = GameObject.Find("EasyDiffToggle");
+            medToggle = GameObject.Find("MediumDiffToggle");
+            hardToggle = GameObject.Find("HardDiffToggle");
+        }
+        
+    }
+    
+    public void SetSettings()
+    {
+        if(easyToggle.GetComponent<Toggle>().isOn)
+        {
+            curDifficulty = gameDifficulty.EASY;
+        }
+        else if(medToggle.GetComponent<Toggle>().isOn)
+        {
+            curDifficulty = gameDifficulty.MEDIUM;
+        }
+        else if(hardToggle.GetComponent<Toggle>().isOn)
+        {
+            curDifficulty = gameDifficulty.HARD;
+        }
+        isSwipeMove = moveSwipeToggle.GetComponent<Toggle>().isOn;
+        isSwipeShoot = shootSwipeToggle.GetComponent<Toggle>().isOn;
+    }
+
+    public void UpdateVolume()
+    {
+        volume = (int)volumeSlider.GetComponent<Slider>().value;
+    }
+}
